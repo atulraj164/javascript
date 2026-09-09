a2b269cff080be971d36133b90f7b984a29b2d99
atulraj164 <136972296+atulraj164@users.noreply.github.com>
2026-09-09 22:27:37 +0530

    Add array operations and average calculation

--- javaScriptClases/tut.js
+++ javaScriptClases/tut.js
@@ -10,3 +10,15 @@ for (let key in Student) {
    let op=`the value for ${key} is ${Student[key]}`;//template literals
     console.log(op);
 }
+
+let arr=[1,2,3,4,5];
+let brr=[1,2,3,4,5];
+console.log(arr);
+console.log(arr.toString());
+console.log(arr.concat(brr));
+console.log(arr.slice(1,3));
+let avg=0;
+for(let i of arr){
+    avg+=i;
+}
+console.log(`avg marks of arr=${avg/arr.length}`);
